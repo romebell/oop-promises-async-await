88ecee4915e98158e50e7389a39c53d226558a7a
Rome Bell <romebell5@gmail.com>
2020-07-23 17:45:11 -0400

    [contructor] Add NBAPlayer functions

--- app.js
+++ app.js
@@ -41,3 +41,33 @@ console.log(apartTwo.rooms);
 console.log(apartOne.rooms);
 
 // Constructor Functions
+function User(name) {
+    // this
+    this.name = name;
+
+    // return this
+}
+
+let adam = new User('Adam');
+let pete = new User('Pete');
+
+console.log(adam.name);
+console.log(pete.name);
+
+function NBAPlayer(name, team, threePointShooter) {
+    // this = {}
+    this.name = name;
+    this.team = team;
+    this.threePointShooter = threePointShooter;
+
+    // return this
+}
+
+let steph = new NBAPlayer('Steph Curry', 'Warriors', true);
+let lebron = new NBAPlayer('Lebron James', 'Lakers', true);
+let james = new NBAPlayer('James Harden', 'Rockets', true);
+console.log(steph);
+console.log(steph.name);
+
+// make a contructor function 
+// Make 3 new variables with that contructor function
